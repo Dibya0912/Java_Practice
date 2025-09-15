4d7d12d8709a2e7d95a5fabac69aa4ad39e7582d
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-15 15:30:58 +0530

    ConditionalsExample2

---
+++ ConditionalsExample2.java
@@ -0,0 +1,13 @@
+public class ConditionalsExample2 {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        if (a > b) {
+            System.out.println(a + " is greater than " + b);
+        } else if (a < b) {
+            System.out.println(b + " is greater than " + a);
+        } else {
+            System.out.println("Both numbers are equal");
+        }
+    }
+}
