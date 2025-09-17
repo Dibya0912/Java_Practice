86069dfe88082434004f171ad269d301a7db9479
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-17 12:34:41 +0530

    WhileLoopExample

---
+++ WhileLoopExample.java
@@ -0,0 +1,11 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        System.out.println("While loop: Printing numbers from 1 to 5");
+
+        int j = 1;
+        while (j <= 5) {
+            System.out.println("j = " + j);
+            j++;
+        }
+    }
+}
