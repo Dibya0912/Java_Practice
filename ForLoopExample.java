b3ed63d0e4d55da85f2b47d46d2766cb9b7f1fe5
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-17 12:32:59 +0530

    ForLoopExample

---
+++ ForLoopExample.java
@@ -0,0 +1,9 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        System.out.println("For loop: Printing numbers from 1 to 5");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
