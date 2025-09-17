e4a30a2b81af965c3d6c58a012fdf3ad672856cf
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-17 12:36:26 +0530

    DoWhileLoopExample

---
+++ DoWhileLoopExample.java
@@ -0,0 +1,11 @@
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        System.out.println("Do-while loop: Printing numbers from 1 to 5");
+
+        int k = 1;
+        do {
+            System.out.println("k = " + k);
+            k++;
+        } while (k <= 5);
+    }
+}
