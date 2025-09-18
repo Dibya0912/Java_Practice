d06767a2e1cccbf20c1a373c8d6098950e089696
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-18 17:05:05 +0530

    PyramidPattern

---
+++ PyramidPattern.java
@@ -0,0 +1,16 @@
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int n = 5; // number of rows
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
