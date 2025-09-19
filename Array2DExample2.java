e1220f7df5af8e4787c34fb8f259f2f99d090d91
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-19 11:48:35 +0530

    Array2DExample2

---
+++ Array2DExample2.java
@@ -0,0 +1,18 @@
+public class Array2DExample2 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Sum of each row:");
+        for (int i = 0; i < matrix.length; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < matrix[i].length; j++) {
+                rowSum += matrix[i][j];
+            }
+            System.out.println("Row " + (i + 1) + " sum = " + rowSum);
+        }
+    }
+}
