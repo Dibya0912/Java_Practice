b0bc5a65cddd8a19cf855da8085da79aa936556a
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-21 17:32:58 +0530

    StringExample1

---
+++ StringExample1.java
@@ -0,0 +1,7 @@
+public class StringExample1 {
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println("Length: " + str.length());
+        System.out.println("Uppercase: " + str.toUpperCase());
+    }
+}
