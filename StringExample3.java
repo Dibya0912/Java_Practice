beda0e4d238dfd97392831d3cbdb77b1d38886d0
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-21 17:36:26 +0530

    StringExample3

---
+++ StringExample3.java
@@ -0,0 +1,7 @@
+public class StringExample3 {
+    public static void main(String[] args) {
+        String str = "Hello World";
+        System.out.println("EqualsIgnoreCase: " + str.equalsIgnoreCase("hello world"));
+        System.out.println("Contains 'World': " + str.contains("World"));
+    }
+}
