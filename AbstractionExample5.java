726f1af1fa759fa2713658fd0f8d116e73556cf4
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-29 19:29:16 +0530

    AbstractionExample5

---
+++ AbstractionExample5.java
@@ -0,0 +1,24 @@
+abstract class Bank {
+    abstract int getRateOfInterest();
+}
+
+class SBI extends Bank {
+    int getRateOfInterest() {
+        return 7;
+    }
+}
+
+class HDFC extends Bank {
+    int getRateOfInterest() {
+        return 8;
+    }
+}
+
+public class AbstractionExample5 {
+    public static void main(String[] args) {
+        Bank b1 = new SBI();
+        Bank b2 = new HDFC();
+        System.out.println("SBI Interest Rate: " + b1.getRateOfInterest() + "%");
+        System.out.println("HDFC Interest Rate: " + b2.getRateOfInterest() + "%");
+    }
+}
