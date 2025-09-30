3e907ac9ac2d72cfa9c0b1d1f9d682a3645dc396
Dibya0912 <pradhandibya0912@gmail.com>
2025-09-30 18:40:49 +0530

    PublicClass

---
+++ PublicClass.java
@@ -0,0 +1,10 @@
+public class PublicClass {
+    public void display() {
+        System.out.println("This is a public class method.");
+    }
+
+    public static void main(String[] args) {
+        PublicClass obj = new PublicClass();
+        obj.display();
+    }
+}
