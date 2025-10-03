f525144a277d05826eece63872d018a06febce6a
Dibya0912 <pradhandibya0912@gmail.com>
2025-10-03 21:02:47 +0530

    ListExample1

---
+++ ListExample1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class ListExample1 {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Mango");
+
+        System.out.println("Fruits: " + fruits);
+    }
+}
