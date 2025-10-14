59f3a8801e98ff172751820562f7ba8ebc4f9685
Dibya0912 <pradhandibya0912@gmail.com>
2025-10-14 21:14:15 +0530

    HashMapExample

---
+++ HashMapExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Apple");
+        map.put(2, "Banana");
+        map.put(3, "Cherry");
+
+        System.out.println("HashMap Elements:");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+
+        System.out.println("Contains key 2: " + map.containsKey(2));
+    }
+}
