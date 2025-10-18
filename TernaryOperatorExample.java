a6e8c5593213834a359cb1229ef8400a0c03a8f4
Dibya0912 <pradhandibya0912@gmail.com>
2025-10-18 19:57:40 +0530

    TernaryOperatorExample

---
+++ TernaryOperatorExample.java
@@ -0,0 +1,7 @@
+public class TernaryOperatorExample {
+    public static void main(String[] args) {
+        int num = 15;
+        String result = (num % 2 == 0) ? "Even" : "Odd";
+        System.out.println("The number " + num + " is " + result);
+    }
+}
