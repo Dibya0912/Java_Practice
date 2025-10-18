d7af978c81c9ac0d0cf81a6441a1e47f8b66e3e3
Dibya0912 <pradhandibya0912@gmail.com>
2025-10-18 19:52:02 +0530

    ArithmeticOperations

---
+++ ArithmeticOperations.java
@@ -0,0 +1,10 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 20, b = 8;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+    }
+}
