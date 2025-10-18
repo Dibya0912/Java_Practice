8d785548b039c19491007f5381390e42cf81d708
Dibya0912 <pradhandibya0912@gmail.com>
2025-10-18 19:57:10 +0530

    AssignmentOperations

---
+++ AssignmentOperations.java
@@ -0,0 +1,11 @@
+public class AssignmentOperations {
+    public static void main(String[] args) {
+        int a = 10;
+        a += 5;  // 15
+        a -= 3;  // 12
+        a *= 2;  // 24
+        a /= 4;  // 6
+        a %= 5;  // 1
+        System.out.println("Final value of a: " + a);
+    }
+}
