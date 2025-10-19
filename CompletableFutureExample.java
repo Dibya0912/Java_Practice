fa47430b35807f0117cb933abfadff792743e042
Dibya0912 <pradhandibya0912@gmail.com>
2025-10-19 19:16:54 +0530

    CompletableFutureExample

---
+++ CompletableFutureExample.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.*;
+
+public class CompletableFutureExample {
+    public static void main(String[] args) throws Exception {
+        CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> "Hello");
+        CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> "World");
+
+        CompletableFuture<String> combined = future1.thenCombine(future2, (a, b) -> a + " " + b);
+        System.out.println(combined.get());
+    }
+}
