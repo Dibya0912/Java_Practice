1299ece02a694dd37b018012ed9a729d7067b6d5
Dibya0912 <pradhandibya0912@gmail.com>
2025-11-04 23:44:49 +0530

    CheckAge

---
+++ CheckAge.java
@@ -0,0 +1,22 @@
+public class CheckAge {
+    public static void main(String[] args) {
+        int age = 18;
+
+        // First conditional check
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote yet.");
+        }
+
+        // Second conditional check with else-if
+        int number = -5;
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
